f87e168c832d3467bb1eda9afb6aab1a13d21c52
javafadu <javafadu@gmail.com>
2023-07-04 00:20:45 +0300

    04jul0020

--- query1.sql
+++ query1.sql
@@ -56,5 +56,19 @@ begin
 	
 end $$;
 	
+-- ******** ROW TYPE ********
+do $$
+declare
+	selected_actor actor%rowtype;
+begin
+	select *
+	from actor
+	into selected_actor
+	where id =1;
+	
+	raise notice 'The actor name is : % %', 
+			selected_actor.first_name, selected_actor.last_name;
+	
+end $$
 	
 	
